707028d679fd5cb745d2710bef470d5ae7c2e316
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-20 20:08:33 +0700

    Switch case+

--- SFML_learn/main.cpp
+++ SFML_learn/main.cpp
@@ -1,24 +1,41 @@
 #include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <thread>
+#include <iostream>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::Uint32 stile = sf::Style::Fullscreen;
+    sf::RenderWindow window(sf::VideoMode(), "SFML works!", stile);
+    sf::CircleShape shape(25.f);
+    shape.setFillColor(sf::Color::Cyan);
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type)
+            {
+            case sf::Event::EventType::Closed:
+                if (event.key.code == sf::Keyboard::Key::Escape) {
+                    window.close();
+                }
+                break;
+
+            case sf::Event::EventType::KeyPressed:
+                if (event.key.code == sf::Keyboard::Key::W) {
+                    window.close();
+                }
+                break;
+            default:
+                break;
+            }
         }
 
         window.clear();
         window.draw(shape);
         window.display();
     }
-
     return 0;
 }
