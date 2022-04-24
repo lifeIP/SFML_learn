852cf730d1dfc01bd4236abc960d2060f5892c72
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-25 01:56:29 +0700

    add TextInput

--- SFML_learn/main.cpp
+++ SFML_learn/main.cpp
@@ -1,12 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <windows.h>
 #include <iostream>
+#include <string>
+#include <iostream>;
 
 int main()
 {
     sf::Uint32 stile = sf::Style::Default;
     sf::RenderWindow window(sf::VideoMode(500,500), "SFML works!", stile);
     sf::CircleShape shape(25.f);
+    sf::String buffer;
     shape.setFillColor(sf::Color::Cyan);
     while (window.isOpen())
     {
@@ -23,6 +26,16 @@ int main()
                     window.setTitle("WOW");
                     Sleep(5000);
                     window.close();
+                }
+                break;
+            case sf::Event::EventType::TextEntered:
+                if (event.text.unicode == 8) {
+                    buffer.erase(buffer.getSize() - 1);
+                    window.setTitle(buffer);
+                }
+                else {
+                    buffer += event.text.unicode;
+                    window.setTitle(buffer);
                 }
                 break;
             default:
